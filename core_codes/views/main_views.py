9be44cdc408e1f8cc9755014224476fac8f8d178
chaeyeongyoon <ui88g216@gmail.com>
2023-07-26 14:07:20 +0900

    linting and pytest

--- core_codes/views/main_views.py
+++ core_codes/views/main_views.py
@@ -1,14 +1,17 @@
 from flask import Blueprint, request, jsonify
 from ..utils import width_height
-bp = Blueprint('main', __name__, url_prefix='/') 
+bp = Blueprint('main', __name__, url_prefix='/')
+
 
 @bp.route('/')
 def assignment():
     return 'chaeyeongyun''s assignment3'
 
+
 @bp.route('/hello')
 def hello_world():
     return 'hello world !'
+
 
 # GET 은 클라이언트에서 서버로 어떠한 리소스로 부터 정보를 요청하기 위해 사용되는 메서드이다. 
 # 예를들면 게시판의 게시물을 조회할 때 쓸 수 있다.'
@@ -16,7 +19,7 @@ def hello_world():
 @bp.route('/echo', methods=["GET"])
 def echo():
     if request.method == "GET":
-        data ={"parameter":"value"}
+        data = {"parameter": "value"}
     return jsonify(data)
 
 # curl -F "file=@/Users/yunchaeyeong/Desktop/mlops/image/cat.jpeg" -X POST http://127.0.0.1:5000/upload_image
@@ -25,4 +28,3 @@ def upload_image():
     if request.method == "POST":
         f = request.files['file']
         return jsonify(width_height(f))
-        
